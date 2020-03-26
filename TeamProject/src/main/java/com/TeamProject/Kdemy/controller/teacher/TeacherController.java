535bf8e198fad43c93c8c84eb6df59f485114657
미래 <@mr02-14-PC>
2020-03-26 15:00:11 +0900

    200326 비디오업로드 및 리스트

--- TeamProject/src/main/java/com/TeamProject/Kdemy/controller/teacher/TeacherController.java
+++ TeamProject/src/main/java/com/TeamProject/Kdemy/controller/teacher/TeacherController.java
@@ -1,31 +1,23 @@
 package com.TeamProject.Kdemy.controller.teacher;
 
-import java.io.FileInputStream;
-import java.io.InputStream;
 import java.util.HashMap;
 import java.util.List;
 
 import javax.annotation.Resource;
 import javax.inject.Inject;
 
-import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.TeamProject.Kdemy.model.teacher.dto.TeacherDTO;
 import com.TeamProject.Kdemy.service.teacher.TeacherService;
-import com.TeamProject.Kdemy.util.MediaUtils;
 import com.TeamProject.Kdemy.util.UploadFileUtils;
 
 @Controller
@@ -47,11 +39,17 @@ public class TeacherController {
 	}
 	//현장 강의를 등록하는 페이지
 	@RequestMapping("offlinePage.do")
+	public String offlinePage() {
+		return "teacher/offline";
+	}
+	
+	
+	@RequestMapping("videoList.do")
 	public ModelAndView typeAList(@RequestParam(defaultValue="1")int curPage) throws Exception {
 		//레코드 갯수 계산
 		int count=teacherService.countTypeAList();
 				//페이지 관련 설정
-	    Pager pager=new Pager(count, curPage);
+		TeacherPager pager=new TeacherPager(count, curPage);
 		int start=pager.getPageBegin();
 		int end=pager.getPageEnd();
 		List<TeacherDTO>list=teacherService.typeAList(start,end);
@@ -63,15 +61,15 @@ public class TeacherController {
 		map.put("count", count);
 		map.put("pager", pager); //페이지 네비게이션을 위한 변수 
 	    //이동 경로 
-		mav.setViewName("teacher/offline");
+		mav.setViewName("teacher/video_list");
 		mav.addObject("map", map); //ModelAndView에 map을 저장
 		return mav;
 	}
 	
 	//동영상 강의를 올리는 페이지
-	@RequestMapping("vedioPage.do")
+	@RequestMapping("videoPage.do")
 	public String vedioPage() {
-		return "teacher/vedio";
+		return "teacher/video";
 	}
 	
 	
@@ -98,7 +96,7 @@ public class TeacherController {
 			dto.setMain_img(main_img);
 			dto.setVideofile(videofile);
 		teacherService.teacher_type1_insert(dto);
-		return "redirect:/teacher/offlinePage.do";
+		return "redirect:/teacher/videoList.do";
 	}
 	
 
---
+++ TeamProject/src/main/webapp/WEB-INF/views/teacher/video_list.jsp
@@ -0,0 +1,69 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+<%@ include file="../include/header.jsp" %>
+<%@ include file="../include/topbar.jsp"%>
+<link rel="stylesheet" href="${path}/include/css/home.css">
+<script src="${path}/include/js/common.js"></script>
+
+
+</head>
+<body>
+<h2>현장 강의 페이지</h2>
+<p>현장 강의를 올리는 페이지 입니다.</p>
+
+<div class="row">
+<br>
+ <c:forEach var="dto" items="${map.list}">
+<h2 class="text-center">${dto.sub_category}</h2>
+
+
+    <div class="col-xs-4 col-sm-4 col-md-3 col-lg-3 text-center">  
+    <a href="${path}/teacher/view.do?lecture_idx=${dto.subject}"></a>
+    <input type="hidden" name="lecture_idx" value="${dto.lecture_idx}">
+     <img src="../upload${dto.main_img}" class="img-rounded img-responsive"> 
+     <video controls="controls" width="100%" preload="metadata">
+        <source src="../upload${dto.videofile}" type="video/mp4">
+      </video>
+     <b>${dto.content}</b><br>
+     <b><fmt:formatNumber value="${dto.price}" pattern="###,###,###"/>원</b><br>
+    </div>
+
+
+ </c:forEach>
+</div>
+
+
+<div class="text-center">
+     <ul class="pagination">
+      <c:if test="${map.page.curBlock > 1 }">
+        <li><a href="#" onclick="list('1')">&lt;&lt;</a></li>
+      </c:if>
+      <c:if test="${map.page.curBlock > 1 }">
+        <li><a href="#" onclick="list('${map.page.prevPage}')">&lt;</a></li>
+      </c:if>
+      <c:forEach var="num" begin="${map.page.blockStart}" end="${map.page.blockEnd}">
+        <c:choose>
+          <c:when test="${num == map.page.curPage}">
+            <li><span style="color: red">${num}</span></li>
+          </c:when>
+          <c:otherwise>
+            <li><a href="#" onclick="list('${num}')">${num}</a></li>
+          </c:otherwise>
+        </c:choose>
+      </c:forEach>
+      <c:if test="${map.page.curBlock < map.page.totBlock}">
+        <li><a href="#" onclick="list('${page.nextPage}')">&gt;</a></li>
+      </c:if>
+      <c:if test="${map.page.curBlock < map.page.totBlock}">
+        <li><a href="#" onclick="list('${map.page.totPage}')">&gt;&gt;</a></li>
+      </c:if>
+  </ul>
+</div>
+
+</body>
+</html>
