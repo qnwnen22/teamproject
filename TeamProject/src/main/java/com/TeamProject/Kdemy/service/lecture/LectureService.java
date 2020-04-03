3328cf8494a1f61044654d3169b6f9e90d56a69e
qnwnen22 <57924805+qnwnen22@users.noreply.github.com>
2020-04-03 14:17:55 +0900

    중간 커밋 [ 이원혁 ]

--- TeamProject/src/main/java/com/TeamProject/Kdemy/service/lecture/LectureService.java
+++ TeamProject/src/main/java/com/TeamProject/Kdemy/service/lecture/LectureService.java
@@ -21,7 +21,16 @@ public interface LectureService {
 	public LectureDTO lecture_list_view(int lecture_idx);
 
 	public int searchCount(String cell_type, String keyword);
+	public int searchCount(String keyword);
 
+	public int countList();
+	public List<LectureDTO> lecture_list(int start, int end);
+	
 	public List<LectureDTO> searchList(String cell_type, String keyword, int start, int end);
+	public List<LectureDTO> searchList(String keyword, int start, int end);
+	
+	public List<LectureDTO> myLectureList(String userid);
+	public void lectureDelete(int lecture_idx);
+	public LectureDTO selectFile(int lecture_idx);
 	
 }
