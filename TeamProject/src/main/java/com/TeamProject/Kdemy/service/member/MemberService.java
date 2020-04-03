79fa49515e93a3b6f6be8d9afe214e0474472ecd
hyun8503 <hyun8503@gmail.com>
2020-04-03 15:10:21 +0900

    다시

--- TeamProject/src/main/java/com/TeamProject/Kdemy/service/member/MemberService.java
+++ TeamProject/src/main/java/com/TeamProject/Kdemy/service/member/MemberService.java
@@ -29,10 +29,5 @@ public interface MemberService {
 	public MemberDTO searchID(MemberDTO dto);
 
 	public void updatePW(MemberDTO dto);
-<<<<<<< HEAD
 
-//	public void deleteFile(String fileName);
-=======
-	
->>>>>>> branch 'master' of https://github.com/qnwnen22/teamproject.git
 }
